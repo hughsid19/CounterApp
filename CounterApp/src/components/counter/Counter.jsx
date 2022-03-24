d0665247201ebc60bb93d4e48b4d1c01156161eb
hughsid19 <prahladtripathi123@gmail.com>
2022-03-24 14:23:00 +0530

    First Commit

---
+++ CounterApp/src/components/counter/Counter.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './Counter.css'
+
+class Counter extends Component{
+ 
+ //Define the initial state in a constructor
+ //state => counter 0
+  constructor(){
+    super();
+    this.state = {
+           counter : 0
+       }
+   }
+  
+  render() {
+    return (
+      <div className="counter">
+      <CounterButton by = {1}  incrementMethod={this.increment} decrementMethod={this.decrement}/>
+      <CounterButton by = {5}  incrementMethod={this.increment} decrementMethod={this.decrement}/>
+      <CounterButton by = {10} incrementMethod={this.increment} decrementMethod={this.decrement}/>
+      <span className="count">{this.state.counter}</span>
+      <div><button className = "reset" onClick={this.reset}>Reset</button></div>
+      </div>
+    );
+  }
+
+  reset = () => {
+    this.setState({counter:0});
+  }
+ 
+ 
+  increment = (by) => { //Update state - counter++
+    this.setState(
+      (prevState) => {
+        return {counter: prevState.counter + by}
+        }
+      ); 
+    }
+
+    decrement = (by) => { //Update state - counter++
+      this.setState(
+        (prevState) => {
+          return {counter: prevState.counter - by}
+          }
+        ); 
+      }
+}
+
+class CounterButton extends Component{
+   
+  
+  constructor(){
+      super();
+    }
+  
+  
+  render = () => {
+    return (
+        <div className="counterbutton">
+          <button onClick = {() => this.props.incrementMethod(this.props.by)}> +{this.props.by}</button>
+          <button onClick = {() => this.props.decrementMethod(this.props.by)}> -{this.props.by}</button>
+        </div>
+      )
+    }
+
+}
+CounterButton.defaultProps = {
+  by:1
+}
+
+CounterButton.propTypes = {
+  by:PropTypes.number
+}
+
+export default Counter;
---
+++ CounterApp/src/components/learning-examples/FirstComponent.jsx
@@ -0,0 +1,12 @@
+//Class Component
+import React,{Component} from 'react';
+
+export default class FirstComponent extends Component {
+    render() {
+      return (
+        <div className="firstComponent">
+          FirstComponet
+        </div>
+      );
+    }
+  }
